f6a72f3a739cf4a370d465411d8e744752c13f7b
Jan Karcz <85148746+Jank201@users.noreply.github.com>
2023-02-25 20:23:49 -0800

    Updated GUI
    Ported the project over to a new library to allow for customization

--- CCApp/app.py
+++ CCApp/app.py
@@ -1,39 +1,29 @@
-import tkinter as tk
-import matplotlib
+import customtkinter as ctk
 from tkinter import filedialog
-import model
 from PIL import Image, ImageTk
-
-matplotlib.use("TkAgg")
+import model
 
 def run_program():
     parameter = parameter_entry.get()
+    if not parameter:
+        text.delete("1.0", "end")
+        text.insert("end", "Please select a file" + "\n")
+        return
     fileExt = parameter.split(".")[-1].lower()
     num = model.run(parameter)
-    show_results(fileExt, num)
+    show_results(fileExt, num, parameter)
 
-def show_results(fileExt, num):
+def show_results(fileExt, num, parameter):
     if fileExt in ['mp4', 'avi', 'mov']:
         image = Image.open("result.png")
         image = image.resize((500, 500), Image.ANTIALIAS)
         image = ImageTk.PhotoImage(image)
 
-        result_window = tk.Toplevel(root)
-        result_window.title("Results")
-        result_window.image = image
-
-        label = tk.Label(result_window, image=image)
-        label.pack(fill=tk.BOTH, expand=True)
-
+        label = ctk.CTkLabel(root,text= "", image=image)
+        label.pack(fill=ctk.BOTH, expand=True)
     else:
-        result_window = tk.Toplevel(root)
-        result_window.title("Results")
-
-        label = tk.Label(result_window, text="Number of People: " + str(num))
-        label.pack(fill=tk.BOTH, expand=True)
-
-    back_button = tk.Button(result_window, text="Close", font=("Helvetica", 14), command=result_window.destroy)
-    back_button.pack(pady=10)
+        label2 = ctk.CTkLabel(root, text= "Number of People in image " + parameter.split("/")[-1] + ": " + str(num),font=("Helvetica", 14))
+        label2.pack(pady=1)
 
 def upload_file():
     file_path = filedialog.askopenfilename()
@@ -45,22 +35,31 @@ def webcam():
     parameter_entry.insert(0, "0")
     run_program()
 
-root = tk.Tk()
-root.title("Model Output")
 
-parameter_label = tk.Label(root, text="Parameter:", font=("Helvetica", 14))
-parameter_label.pack(pady=10)
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("dark-blue")
 
-parameter_entry = tk.Entry(root, font=("Helvetica", 14))
-parameter_entry.pack(pady=10)
+root = ctk.CTk()
 
-upload_button = tk.Button(root, text="Upload File", font=("Helvetica", 14), command=upload_file)
+root.title("Crowd Counting App")
+
+parameter_label = ctk.CTkLabel(root, text="file:", font=("Helvetica", 14))
+parameter_label.pack(pady=1)
+
+parameter_entry = ctk.CTkEntry(root, font=("Helvetica", 14))
+parameter_entry.pack(pady=1)
+
+upload_button = ctk.CTkButton(root, text="Select file", font=("Helvetica", 14), command=upload_file)
 upload_button.pack(pady=10)
 
-run_button = tk.Button(root, text="Run", font=("Helvetica", 14), command=run_program)
+run_button = ctk.CTkButton(root, text="Run", font=("Helvetica", 14), command=run_program)
 run_button.pack(pady=10)
 
-web_button = tk.Button(root, text="Webcam", font=("Helvetica", 14), command=webcam)
+web_button = ctk.CTkButton(root, text="Webcam", font=("Helvetica", 14), command=webcam)
 web_button.pack(pady=10)
+
+text = ctk.CTkTextbox(root, font=("Helvetica", 20), height=5)
+text.pack(padx=10, pady=10)
+
 
 root.mainloop()
